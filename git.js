f78b94145442d3bad00adea1300d486fbaa85ff2
daijun <daijun4@staff.sina.com.cn>
2013-03-07 18:16:26 +0800

    git.js

--- git.js
+++ git.js
@@ -1,3 +1,4 @@
+console.log("start")
 function getMainbody() {
     bodylength = document.getElementsByTagName("body")[0].textContent.length;
     var textlist = document.getElementsByTagName("div");
@@ -12,10 +13,7 @@ function getMainbody() {
             }
         }
     } 
-    //var resultstring =  textlist[minindex].textContent;
-    //var pattern = /\n{2,}/
-    //var result = resultstring.replace(pattern,"");
-   // return resultstring; 
+    var resultstring =  textlist[minindex].textContent;
    return textlist[minindex];
 }
 
@@ -29,11 +27,72 @@ function addItem() {
     node.setAttribute("style","display:none");
     node.style.overflow = "auto";
     node.style.height = "400px"
-console.log(node);
     body.appendChild(node); 
 }
 
-window.onload = addItem;
+function showmask() {
+    mask = document.createElement("div");
+    var width = $(document).width();
+    var height = $(document).height();
+    mask.id = "mask";
+    mask.style.cssText = "position:absolute;z-index:5;width:"+width+"px;height:"+height+"px;background:#000;filter:alpha(opacity=30);opacity:0.3;top:0;left:0";
+    $("#mask").click(function() {
+        console.log('click');
+        $("#mask").remove();
+    });  
+    document.body.appendChild(mask); 
+    
+}
+
+//show table
+function showdiv() {
+   var superbox = document.createElement("div"); 
+   superbox.id  = "superbox";
+   superbox.style.cssText = "display:none;position:absolute;z-index:40; border:solid 1px #79BCFF; background-color: #FFFFFF;  width:900px;height:450px;";
+   var node = getMainbody().cloneNode(true);
+   node.id = "linxhometext";
+   node.style.overflow = "auto";
+   node.style.height = "400px"
+   node.style.width = "900px"
+   superbox.appendChild(document.createElement('br'));
+   superbox.appendChild(node);
+ 
+   var submitbutton = document.createElement("p");
+   submitbutton.id = "submit";  
+   submitbutton.style.cssText = "cursor:pointer;height:30px;background-color:#EEF2FB;text-align:center";
+   submitbutton.innerHTML = "<a style='width:900px;font-size:18px;'><strong><span style='color:#000000'>submit</span></strong></a>"; 
+   superbox.appendChild(submitbutton);
+   
+   document.body.appendChild(superbox); 
+   var o  = document.getElementById("superbox");
+   o.style.display="block";
+   o.style.top="253px";
+   var width = $(document).width();
+   var left = (width-800)/2;
+   o.style.left = left+"px";
+
+
+}
+
+function init() {
+    //抽取内容
+    getMainbody();
+    //显示蒙层
+    showmask();
+    //显示内容
+    showdiv();
+    $("#submit").click(function(){
+       document.getElementById("superbox").style.display='none';
+       document.getElementById("mask").style.display='none';
+    });
+    
+}
+function showagain() {
+    document.getElementById("superbox").style.display='';
+    document.getElementById("mask").style.display='';
+
+}
+init();
 //window.onload = getMainbody;
 
 
